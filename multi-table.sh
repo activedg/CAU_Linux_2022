e9eae7d1a356c648ee2c71c9418c436c13334ae8
active_dg <nerw173@gmail.com>
2022-10-05 17:35:44 +0900

    multi-table update

--- multi-table.sh
+++ multi-table.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+if [ -z $1 -o -z $2 ];
+then
+	echo "Invalid Input"
+	exit 0
+elif [ $1 -le 0 -o  $2 -le 0 ];
+then
+	echo "Input must be greater than 0"
+	exit 0
+fi
 
-echo "hello world"
+for i in $(seq 1 $1)
+do
+	for j in $(seq 1 $2)
+	do
+		printf  "${i}*${j}=`expr $i \* $j` \t"
+	done
+	printf "\n"
+done
 
 exit 0
